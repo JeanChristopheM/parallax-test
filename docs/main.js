b771cb08b895f112562392bc070bbb2fb8d2438c
Jean-Christophe Molhant <jean.christophe.molhant@gmail.com>
2021-09-17 15:58:28 +0200

    pushing to have online presence

---
+++ docs/main.js
@@ -0,0 +1,21 @@
+let stylesheet = document.styleSheets[0];
+let paraCSS;
+let paraBeforeCSS;
+let paraAfterCSS;
+
+for (item of stylesheet.rules) {
+    if (item.selectorText == '.para') paraCSS = item;
+    if (item.selectorText == '.para::before') paraBeforeCSS = item;
+}
+
+console.log(document.querySelector('.para').getBoundingClientRect().y);
+console.log(window.innerHeight);
+console.log(document.querySelector('.para').offsetHeight);
+console.log(window.innerHeight + document.querySelector('.para').getBoundingClientRect().y);
+
+window.addEventListener('scroll', () => {
+    paraBeforeCSS.style.height = `${(document.querySelector('.para').offsetHeight - document.querySelector('.para').getBoundingClientRect().y) / 2}px`;
+});
+
+let styles = [paraBeforeCSS, paraAfterCSS];
+console.log(styles);
